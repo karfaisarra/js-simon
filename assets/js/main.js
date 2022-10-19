da40ebe512bc00e1cdcd879d784abd41c31a75c1
karfaisarra <karfaisarra@gmail.com>
2022-10-19 17:19:03 +0200

    add another setTimeout functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,13 +27,46 @@ const ulElement = document.querySelector('.numberList');
 //creo elemento li e inserisco il numero presente nel arrey
 let liElement;
 for (let i = 0; i < numbersArray.length; i++) {
-    liElement = `<li>${numbersArray[i]} </li>`
+    liElement = `<li>${numbersArray[i]} </li>`;
     ulElement.innerHTML += liElement;
 }
 
 //Dopo 30 secondi i numeri scompaiono
 setTimeout( function tempoRimanente() {
     ulElement.innerHTML = '';
-}, 10000)
+}, 30000);
+//l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt()
+let numeriUtente = [];
+setTimeout( function risposteUtente() {
+    for (let i = 0; i < 5; i++) {
+        const numero = Number(prompt('inserici un numero'));
+        numeriUtente.push(numero);
+    }
+    
+}, 31000);
+//console.log(numeriUtente); 
+let conta = 0;
+setTimeout(function punteggio(){
+    for (let i = 0; i < numbersArray.length; i++) {
+        const elementNumberArrey = numbersArray[i];
+        for (let i = 0; i < numeriUtente.length; i++) {
+            const elementNumeriUtente = numeriUtente[i];
+            if (elementNumberArrey === elementNumeriUtente) {
+                conta++;
+            }
+        }
+        
+    }
+    if (conta=== 5) {
+        console.log('Ottima!! hai indovinato tutte le risposte' );
+        ulElement.innerHTML = `Ottima!! hai indovinato tutte le risposte`;
 
+    } else if( conta === 0){
+        console.log('Mi dispiace! Hai perso!' );
+        ulElement.innerHTML = `Mi dispiace! Hai perso!`;
+    }else{
+        console.log('Bravo! hai indovinato', conta );
+        ulElement.innerHTML = `Bravo! hai indovinato: ${conta} numeri`;
 
+    }
+}, 32000)
